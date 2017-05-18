b90ffa9c919bef1b2d2a07943553854bed7df13e
Ekaterina Troshkova <e-troshkova@inbox.ru>
2017-05-18 16:18:29 +0800

    initial commit

---
+++ app/src/main/java/com/troshkova/portfolioprogect/examsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.troshkova.portfolioprogect.examsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
